622551339cf27e943195c837a17b5e1ea78ea417
shetty1807 <shetty18naveen@gmail.com>
2025-06-09 14:29:34 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Jenkins build successfully complete");
+});
+
+// const port = process.env.PORt || 3000;
+// app.listen(port, () => console.log(app listening on port ${port}));
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
